b4eb4f4d1bacf600f56941cb958ba7deb36286a7
Arthur Caron <arthurcaron@me.com>
2021-08-28 15:56:11 +0200

    streamlit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import librosa
+import glob
+import os
+from helper import draw_embed, create_spectrogram, read_audio, record, save_record
+
+dir = 'samples/'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))    
+
+st.sidebar.image('Ai-Motion.png', width=300)
+
+st.header("Record your own voice")
+
+filename = st.text_input("Choose a filename: ")
+
+if st.button(f"Click to Record"):
+    if filename == "":
+        st.warning("Choose a filename.")
+    else:
+        record_state = st.text("Recording...")
+        duration = 8  # seconds
+        fs = 48000
+        myrecording = record(duration, fs)
+        record_state.text(f"Saving sample as {filename}.mp3")
+
+        path_myrecording = f"./samples/{filename}.mp3"
+
+        save_record(path_myrecording, myrecording, fs)
+        record_state.text(f"Done! Saved sample as {filename}.mp3")
+
+        st.audio(read_audio(path_myrecording))
+
+        fig = create_spectrogram(path_myrecording)
+        st.pyplot(fig)
