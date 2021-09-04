c80080d1824cb7cda14755e8fabfe41d531dad6d
Arthur Caron <arthurcaron@me.com>
2021-09-04 10:36:37 +0200

    ok

--- app.py
+++ app.py
@@ -14,36 +14,30 @@ st.sidebar.image('Ai-Motion.png', width=300)
 
 st.header("Record your own voice")
 
-filename = st.text_input("Choose a filename: ")
+filename = "recording"
 
 if st.button(f"Click to Record"):
-    if filename == "":
-        st.warning("Choose a filename.")
-    else:
-        record_state = st.text("Recording...")
-        duration = 8  # seconds
-        fs = 48000
-        myrecording = record(duration, fs)
-        record_state.text(f"Saving sample as {filename}.mp3")
+    
+    record_state = st.text("Recording...")
+    duration = 8  # seconds
+    fs = 48000
+    myrecording = record(duration, fs)
+    record_state.text(f"Saving sample as {filename}.mp3")
 
-        path_myrecording = f"./samples/{filename}.mp3"
+    path_myrecording = f"./samples/{filename}.mp3"
 
-        save_record(path_myrecording, myrecording, fs)
-        record_state.text(f"Done! Saved sample as {filename}.mp3")
+    save_record(path_myrecording, myrecording, fs)
+    record_state.text(f"Done! Saved sample as {filename}.mp3")
 
-        st.audio(read_audio(path_myrecording))
+    st.audio(read_audio(path_myrecording))
 
-        fig = create_spectrogram(path_myrecording)
-        st.pyplot(fig)
+    fig = create_spectrogram(path_myrecording)
+    st.pyplot(fig)
         
-        import subprocess
+    import subprocess
   
-        # convert mp3 to wav file
-        subprocess.call(['ffmpeg', '-i', path_myrecording,
-                 'converted_to_wav_file.wav'])
+    # convert mp3 to wav file
+    subprocess.call(['ffmpeg', '-i', path_myrecording,
+             'converted_to_wav_file.wav'])
         
-        dst = "test.wav"
-
-        #   convert wav to mp3                                                            
-        sound = AudioSegment.from_file(path_myrecording)
-        sound.export(dst, format="wav")
+    
