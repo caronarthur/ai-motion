019e14a10f32cafc60133b35e856644179856844
Arthur Caron <arthurcaron@me.com>
2021-09-04 14:45:11 +0200

    ok

--- speech_emotion_reco/combine_models.py
+++ speech_emotion_reco/combine_models.py
@@ -15,8 +15,6 @@ def combine_predict(sound):
 
     proba1.rename(columns={0: "angry", 1: "disgust",2:"fear",3:"happy",4:"neutral",5:"sad"}, inplace=True)
     proba2.rename(columns={0: "happy", 1: "sad",2:"fear",3:"disgust",4:"angry",5:"neutral"}, inplace=True)
-
-
     
     combined_proba= proba1+proba2
 
