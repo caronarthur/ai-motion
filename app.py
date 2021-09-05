5f972275454a907fd6ebcea36e8ed66112e2dc88
rvo1994 <72031698+rvo1994@users.noreply.github.com>
2021-09-05 11:04:56 +0200

    new version app.py

--- app.py
+++ app.py
@@ -10,13 +10,18 @@ from speech_emotion_reco.mateo_preprocess import sound_to_number
 from speech_emotion_reco.raph_data import get_array
 import requests
 
-
 # page conf
 st.set_page_config(
     page_title="AI-motion",
-    page_icon='speech_emotion_reco/data/emoji/robot.png',
+    page_icon='speech_emotion_reco/data/emoji/favicon.png',
     layout="centered", # wide
     initial_sidebar_state="auto") # collapsed
+
+
+st.image('speech_emotion_reco/data/emoji/logo.png')
+
+title = '<b style="font-family:IBM plex sans; color:#EB7B67; font-size: 40px; {text-align: left;}">Let me detect how you feel:</b>'
+st.markdown(title, unsafe_allow_html=True)
 
 dir = 'samples/'
 for f in os.listdir(dir):
@@ -24,8 +29,6 @@ for f in os.listdir(dir):
 
 #st.image('Ai-Motion.png', width=300)
 
-st.image('speech_emotion_reco/data/emoji/robot.png')
-st.header("AI-motion will predict how you feel:")
 st.text("")
 
 filename = "recording"
@@ -33,7 +36,7 @@ filename = "recording"
 if st.button(f"Click to record your voice"):
     
     record_state = st.text("Recording...")
-    duration = 8  # seconds
+    duration = 1  # seconds
     fs = 48000
     myrecording = record(duration, fs)
     #record_state.text(f"Saving sample as {filename}.mp3")
@@ -41,35 +44,45 @@ if st.button(f"Click to record your voice"):
     path_myrecording = f"./samples/{filename}.mp3"
 
     save_record(path_myrecording, myrecording, fs)
-    record_state.text(f"Voice recorded!")
+    record_state.text(f"Voice recorded! Please wait a moment, AI-motions is analyzing your voice...")
     st.text("")
     st.audio(read_audio(path_myrecording))
     st.text("")
-    st.subheader("You are most-likely feeling...")
-    st.text("")
+    
+    api_file_upload_url = 'http://127.0.0.1:8000/'
+    send_files = {'audioFile': ('music.mp3', path_myrecording, 'audio/mpeg')}
+    response = requests.post(api_file_upload_url, files=send_files)
+    emotion1, proba1 = response['emotion1'][0], round(response['emotion1'][1],2)*100
+    emotion2, proba2 = response['emotion2'][0], round(response['emotion2'][1],2)*100
+    emotion3, proba3 = response['emotion3'][0], round(response['emotion3'][1],2)*100
     
     c1, c2, c3 = st.columns(3)
-    
-    c1.image('speech_emotion_reco/data/emoji/sad.png')
+    with c1:
+        st.image(f'speech_emotion_reco/data/emoji/{emotion1}.png')
+        st.subheader(f'{emotion1} ({proba1}%)')
 
     with c2:
-        st.image('speech_emotion_reco/data/emoji/happy.png')
+        st.image(f'speech_emotion_reco/data/emoji/{emotion2}.png')
+        st.subheader(f'...or {emotion2} ({proba2}%)')
+        
+    with c3:
+        st.image(f'speech_emotion_reco/data/emoji/{emotion3}.png')
+        st.subheader(f'...or {emotion3} ({proba3}%)')
+            
+    record_state.text(f"Done!")
 
-    c3.image('speech_emotion_reco/data/emoji/disgust.png')     
-
-    c1.subheader("sad (78%)")
-
-    with c2:
-        st.subheader("happy (23%)")
-
-    c3.subheader("disgust (10%)")  
-    
-st.text("") 
-st.text("") 
+st.text("")
 st.text("")
 st.text("") 
-st.caption('This speech emotion recognition app was built by [mlorantdourte](https://github.com/mlorantdourte), [caronarthur](https://github.com/caronarthur/speech_emotion_reco/commits?author=caronarthur) and [rvo1994](https://github.com/rvo1994)')
-#st.caption('Check out the full project on [Github](https://github.com/caronarthur/speech_emotion_reco)')
+
+#caption = '<p style="font-family:IBM plex sans; color:Grey; font-size: 10px; {text-align: center;}">This speech emotion recognition app was built by [mlorantdourte](https://github.com/mlorantdourte), [caronarthur](https://github.com/caronarthur) and [rvo1994](https://github.com/rvo1994)</p>'
+#caption = <a href="https://www.w3schools.com/">Visit W3Schools.com!</a>
+
+#st.markdown(caption, unsafe_allow_html=True)
+
+st.caption('This speech emotion recognition app was built by [mlorantdourte](https://github.com/mlorantdourte), [caronarthur](https://github.com/caronarthur) and [rvo1994](https://github.com/rvo1994)')
+
+    #st.caption('Check out the full project on [Github](https://github.com/caronarthur/speech_emotion_reco)')
 
       
     #fig = create_spectrogram(path_myrecording)
@@ -83,9 +96,7 @@ st.caption('This speech emotion recognition app was built by [mlorantdourte](htt
     
     #sound_array = get_array("samples/converted_to_wav_file.wav")
     
-    #api_file_upload_url = 'http://127.0.0.1:8000/'
-    #send_files = {'audioFile': ('music.mp3', path_myrecording, 'audio/mpeg')}
-    #r = requests.post(api_file_upload_url, files=send_files)
+
     
     
     
