1edbd41cd472812da5452e27962bb4c0d0dbe607
mateo lorant <mateo.lorant@gmail.com>
2021-09-05 10:32:19 +0200

    change of combined

--- speech_emotion_reco/combine_models.py
+++ speech_emotion_reco/combine_models.py
@@ -15,10 +15,14 @@ def combine_predict(X_1, X_2):
     proba1.rename(columns={0: "angry", 1: "disgust",2:"fear",3:"happy",4:"neutral",5:"sad"}, inplace=True)
     proba2.rename(columns={0: "happy", 1: "sad",2:"fear",3:"disgust",4:"angry",5:"neutral"}, inplace=True)
     
-    combined_proba= proba1+proba2
-
-    emotion_predict=combined_proba[['angry','disgust','fear',"happy",'neutral','sad']].idxmax(axis=1)
-    return {"prediction":emotion_predict}
+    combined_proba= (proba1+proba2)/2
+    combined_proba.sort_values(by=0,ascending=False, axis=1, inplace=True)
+    proba_first_three= combined_proba.iloc[:,0:3]
+    #combined_proba = list(combined_proba).sort(reverse=True)
+    #return combined_proba[0:3]
+    #emotion_predict=combined_proba[['angry','disgust','fear',"happy",'neutral','sad']].idxmax(axis=1)
+    #return {"prediction":emotion_predict}
+    return proba_first_three.to_dict(orient="list")
   
 
 
