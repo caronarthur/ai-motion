64b17b2769f1a88ac1a32309406b3cf43af39e20
rvo1994 <72031698+rvo1994@users.noreply.github.com>
2021-09-15 19:38:13 +0200

    code ready to be deployed to production

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
-import os
-#from helper import create_spectrogram, read_audio, record, save_record
-from speech_emotion_reco.mateo_preprocess import sound_to_number
-from speech_emotion_reco.raph_data import get_array
 import requests
 import base64
 
 # page conf
 st.set_page_config(
     page_title="AI-motion",
-    page_icon='speech_emotion_reco/images/favicon.png',
+    page_icon='images/favicon.png',
     initial_sidebar_state="auto") # collapsed
 
 def local_css(file_name):
@@ -21,7 +17,7 @@ local_css("style.css")
 st.markdown(
     f"""
     <div class="container">
-        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open("speech_emotion_reco/data/emoji/logo.png", "rb").read()).decode()}">
+        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open("images/logo.png", "rb").read()).decode()}">
     </div>
     """,
     unsafe_allow_html=True
@@ -29,57 +25,30 @@ st.markdown(
 
 st.text("")
 
-title = '<b style="font-family:IBM plex sans; font-size: 25px; {text-align: center;}">Let\'s see how you feel:</b>'
+title = '<b style="font-family:IBM plex sans; color:#EB7B67; font-size: 25px; {text-align: center;}">Let\'s see how you feel:</b>'
 st.markdown(title, unsafe_allow_html=True)
 
-dir = 'samples/'
-for f in os.listdir(dir):
-    os.remove(os.path.join(dir, f))    
+uploaded_file = st.file_uploader("Upload Files",type=['wav'])
 
-#st.image('Ai-Motion.png', width=300)
-
-filename = "recording"
-
-if st.button(f"Record your voice"):
-    
-    record_state = st.text("Recording...")
-    duration = 8  # seconds
-    fs = 48000
-    myrecording = record(duration, fs)
-    #record_state.text(f"Saving sample as {filename}.mp3")
-
-    path_myrecording = f"./samples/{filename}.mp3"
-    #path_converted = f"./samples/{filename}.wav"
-
-    save_record(path_myrecording, myrecording, fs)
-    record_state.text(f"Voice recorded! Please wait a moment, AI-motion is analyzing your voice...")
-    st.audio(read_audio(path_myrecording))
-    
+if uploaded_file is not None:
+    files = {'my_file': uploaded_file}
     url = "https://ai-motion-api-g6zof5oyea-ew.a.run.app/upload/"
-    files = {'my_file': open(path_myrecording, 'rb')}
-    #r= requests.post(url, files=files)
-    
-    #subprocess.call(['ffmpeg', '-i', path_myrecording,path_converted])  
-    #url = "https://ai-motion-api-g6zof5oyea-ew.a.run.app/upload"
-    #files = {'my_file': open(path_converted, 'rb')}
-
-    #st.text(requests.post(url, files=files))
+    st.audio(uploaded_file)
     response = requests.post(url, files=files).json()
     emotion1, proba1 = response['emotion1'][0], round(response['emotion1'][1]*100)
     emotion2, proba2 = response['emotion2'][0], round(response['emotion2'][1]*100)
     emotion3, proba3 = response['emotion3'][0], round(response['emotion3'][1]*100)
-    
-    title = '<b style="font-family:IBM plex sans; font-size: 25px; {text-align: center;}">You are most-likely feeling:</b>'
+    title = '<b style="font-family:IBM plex sans; color:#EB7B67; font-size: 25px; {text-align: center;}">You are most-likely feeling:</b>'
     st.markdown(title, unsafe_allow_html=True)
-
-    c1, c2, c3 = st.columns(3)
+    
+    c1, c2, c3 = st.columns(3)  
     
     with c1:
         #st.image(f'speech_emotion_reco/data/emoji/{emotion1}.png')
         st.markdown(
         f"""
         <div class="container">
-        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(f"speech_emotion_reco/data/emoji/{emotion1}.png", "rb").read()).decode()}">
+        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(f"images/{emotion1}.png", "rb").read()).decode()}">
         </div>
         """,
         unsafe_allow_html=True)
@@ -90,7 +59,7 @@ if st.button(f"Record your voice"):
         st.markdown(
         f"""
         <div class="container">
-        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(f"speech_emotion_reco/data/emoji/{emotion2}.png", "rb").read()).decode()}">
+        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(f"images/{emotion2}.png", "rb").read()).decode()}">
         </div>
         """, 
         unsafe_allow_html=True)
@@ -101,13 +70,11 @@ if st.button(f"Record your voice"):
         st.markdown(
         f"""
         <div class="container">
-        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(f"speech_emotion_reco/data/emoji/{emotion3}.png", "rb").read()).decode()}">
+        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(f"images/{emotion3}.png", "rb").read()).decode()}">
         </div>
         """,
         unsafe_allow_html=True)
         st.subheader(f'...or {emotion3} ({proba3}%)')
-
-    record_state.text(f"Done!")
 
 st.text("")
 st.text("")
@@ -115,38 +82,5 @@ st.text("")
 caption = '<smaller style="font-family:IBM plex sans; color:#8F8E9A; font-size: 15px; {text-align: center;}">This speech emotion recognition app was built by [mlorantdourte](https://github.com/mlorantdourte), [caronarthur](https://github.com/caronarthur) and [rvo1994](https://github.com/rvo1994).</smaller>'
 st.markdown(caption, unsafe_allow_html=True)
 
-
-    #st.caption('Check out the full project on [Github](https://github.com/caronarthur/speech_emotion_reco)')
-
-      
-    #fig = create_spectrogram(path_myrecording)
-    #st.pyplot(fig)
-  
-    # convert mp3 to wav file
-    #subprocess.call(['ffmpeg', '-i', path_myrecording,
-    #         'samples/converted_to_wav_file.wav'])
-    #
-    #sound_number = sound_to_number("samples/converted_to_wav_file.wav")
-    
-    #sound_array = get_array("samples/converted_to_wav_file.wav")
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-
-    
-    
-    
-    
-    
-    
     
     
